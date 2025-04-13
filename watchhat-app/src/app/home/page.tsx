12fcddadf2c30705020999520dc0589fd2fab546
Shillky <hillsky08@gmail.com>
2025-04-13 15:17:58 -0400

    Adding search to home page

--- watchhat-app/src/app/home/page.tsx
+++ watchhat-app/src/app/home/page.tsx
@@ -1,7 +1,13 @@
 const Home = () => {
     return(
-        <div className="flex justify-center items-center min-h-screen">
-            <h1>Hello There</h1>
+        <div className="w-screen h-screen flex justify-center items-center px-4">
+            <div className="w-full max-w-lg min-w-[200px]">
+                <div className="relative w-full">
+                    <input className="w-full bg-transparent bg-white text-md text-[#244855] font-bold border rounded-md pl-3 pr-28 py-2 focus:outline-[#E64833] hover:border-slate-300 shadow-sm focus:shadow" placeholder="Search for Titles"/>
+                    <button className="absolute top-1 right-1 flex items-center rounded bg-[#90AEAD] py-1 px-2.5 border border-transparent text-center text-md font-bold text-[#244855] shadow-sm hover:shadow hover:bg-[90AEAD] active:scale-90" type="button">Search
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
