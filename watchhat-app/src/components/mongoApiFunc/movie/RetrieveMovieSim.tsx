0a90465ab9cd32472a6e96ae133a4625221b9f9f
fidelA2021 <145623542+fidelA2021@users.noreply.github.com>
2025-04-19 20:56:38 -0400

    Added retrieveMovieSim which will be used for similarity.

---
+++ watchhat-app/src/components/mongoApiFunc/movie/RetrieveMovieSim.tsx
@@ -0,0 +1,36 @@
+
+/**
+ * This function will get all movies from the database. It pushes in infromation just
+ * related to cast ids, genre ids, and movie id. 
+ * 
+ * @param updateArray the array to have the data pushed to.
+ */
+export default async function RetrieveMovieSim (updateArray: any[]) {
+    console.log("running RetrieveMovieSim");
+    const url = `http://localhost:3000/api/movies`
+    try {
+      //get response to api url
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response for update cast was not ok');
+      }
+      const result = await response.json();
+      let {id, genre_ids, cast}= result.movies[0];
+      const temp = result.movies.map((movie: any) => {
+        const { id, genre_ids, cast } = movie;
+        return { id, genre_ids, cast };
+      });
+      updateArray.push(...temp);
+      //console.log(result.movies.length);
+      /*for (var i = 0; i < result.movies.length; i++) {
+        //console.log(result.movies[i]);
+        const movieId = result.movies[i].id;
+
+        await GetActorData(movieId);
+      }*/
+
+      console.log('Ending RetrieveMovieSim');
+    } catch (error) {
+      console.error('Error in RetrieveMovieSim!', error);
+    }
+  };
